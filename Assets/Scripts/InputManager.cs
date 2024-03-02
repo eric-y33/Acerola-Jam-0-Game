b7b160232815ed64fcd9852d47036dd8eb15925c
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-03-01 21:39:31 -0800

    Initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+    public PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    void Start()
+    {
+        // Cursor.visible = false;
+    }
+
+    void FixedUpdate()
+    {
+        // tell playermotor to move with value from movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
