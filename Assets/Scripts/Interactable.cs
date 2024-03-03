d423d58a37f3c750b3b70cdc25bea8a8ede3e11b
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-03-03 12:02:30 -0800

    added InteractionEvents

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-
+    public bool useEvents;
     // Message displayed to player when looking at an interactable
     public string promptMessage;
 
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
         Interact();
     }
 
