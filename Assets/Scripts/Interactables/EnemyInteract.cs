74e255c64fa3b1bfaa4e428b24e895cf48d82996
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-03-06 21:15:11 -0800

    randomized enemy interact prompts

--- Assets/Scripts/Interactables/EnemyInteract.cs
+++ Assets/Scripts/Interactables/EnemyInteract.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyInteract : Interactable
 {
     public ProgressBar progressBar;
+    private string[] prompts = new string[]{"[KILL]", "[DESTROY]", "[RELEASE]", "[ABANDON]"};
 
     // Start is called before the first frame update
     void Start()
     {
         // progressBar = GameObject.Find("Health");
         progressBar.decreaseCurrent();
-        promptMessage = "Hello";
+        int randomPromptIndex = Random.Range(0,(prompts.Length - 1));
+        promptMessage = prompts[randomPromptIndex];
         
     }
 
