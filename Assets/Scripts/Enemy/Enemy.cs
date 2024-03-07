ed3ea07bbb472981a8041bcc421284450ff30971
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-03-06 20:29:03 -0800

    made basic random enemy spawning

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,16 +5,17 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
     private NavMeshAgent agent;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
     }
 
     void Update()
     {
-        agent.destination = player.position;
+        agent.destination = player.transform.position;
     }
 }
---
+++ Assets/Scripts/Enemy/GenerateEnemies.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+
+    public GameObject enemy;
+    private int xPos;
+    private int zPos;
+    private int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(35, 45) * RandomSign();
+            zPos = Random.Range(-40, 40);
+            Instantiate(enemy, new Vector3(xPos, 1.6f, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+
+    int RandomSign() {
+    if (Random.Range(0, 2) == 0) {
+        return -1;
+    }
+        return 1;
+    }
+}
