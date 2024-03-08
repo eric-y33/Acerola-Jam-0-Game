84349d39715bec4b3514ed5a0dc6cb94eb3713a2
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-03-07 20:53:43 -0800

    fades to outro when complete

--- Assets/Scripts/SceneStateChecker.cs
+++ Assets/Scripts/SceneStateChecker.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneStateChecker : MonoBehaviour
 {
 
     public ProgressBar extractionProgress;
     public ProgressBar health;
+    public SceneChanger sceneChanger;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,15 @@ public class SceneStateChecker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health.current <= 0) 
+        {
+            sceneChanger.FadeToLevel(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (extractionProgress.current >= extractionProgress.maximum)
+        {
+            // Do other ending stuff before fading to level
+            sceneChanger.FadeToNextLevel();
+        }
     }
 }
