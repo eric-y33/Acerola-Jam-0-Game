f8345e8d9ae95a16918487cea90424ca151d5ddf
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-03-11 21:53:46 -0700

    adjusted depth of field

--- Assets/Scripts/Enemy/GenerateEnemies.cs
+++ Assets/Scripts/Enemy/GenerateEnemies.cs
@@ -18,7 +18,7 @@ public class GenerateEnemies : MonoBehaviour
     private bool fourthWave;
     private bool fifthWave;
 
-    private string[] prompts = new string[]{"[DESTROY]", "[RELEASE]", "[ABANDON]"};
+    private string[] prompts = new string[]{"[KILL]", "[FORGET]", "[ABANDON]"};
 
     // Start is called before the first frame update
     void Start()
